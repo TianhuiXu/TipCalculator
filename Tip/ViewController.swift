0be9cf97291c2285f5adae69f8dba7755775b4db
Tianhui Xu <45302287+TianhuiXu@users.noreply.github.com>
2020-09-06 14:31:01 +0800

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Tianhui Xu on 2020/9/6.
+//  Copyright © 2020 Tianhui Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
